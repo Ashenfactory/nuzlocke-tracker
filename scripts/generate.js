abd33a78639d1dabe3840fe6f8b008379be57480
Ashenfactory <Ashenfactory@gmail.com>
2023-01-09 04:33:48 +0100

    Gen 9 sprites by Ezerart

--- scripts/generate.js
+++ scripts/generate.js
@@ -27,6 +27,13 @@ for (let i = 1; i <= pkmnCount; i++) {
 }
 
 additionalPkmnData = JSON.parse(fs.readFileSync('scripts/additional-pokemon.json', 'utf-8'));
+
+additionalPkmnData.forEach((pkmn, index) => {
+	spriteSheetCSS += '.pkmn.' + pkmn.value + ':before {\n' +
+		'	background-position: 0 -' + (index + pkmnCount + 1) * imageHeight + 'px;\n' +
+		'}\n\n';
+});
+
 dataJS = dataJS.concat(additionalPkmnData);
 
 excludedPkmn = JSON.parse(fs.readFileSync('scripts/excluded-pokemon.json', 'utf-8'));
@@ -42,7 +49,7 @@ for (const [game, ids] of Object.entries(excludedPkmn)) {
 	});
 }
 
-fs.writeFileSync('images.txt', images);
+fs.writeFileSync('images.txt', images + "'scripts/additional-pokemon.png'");
 execSync('convert @images.txt -append img/sprites.png');
 fs.unlinkSync('images.txt');
 
@@ -98,7 +105,6 @@ fomanticAssets.forEach((asset => {
 fs.writeFileSync('src/css/fomantic.css', fomanticCSS);
 fs.writeFileSync('src/js/fomantic.js', fomanticJS);
 fs.copyFileSync('node_modules/jquery/dist/jquery.js', 'src/js/jquery.js');
-fs.copyFileSync('node_modules/file-saver/dist/FileSaver.js', 'src/js/FileSaver.js');
 
 fs.writeFileSync('src/css/sprites.css', spriteSheetCSS);
 fs.writeFileSync('src/js/pokemon.js', 'const pkmnData = ' + (JSON.stringify(dataJS, null, '\t')).replace(/"([^"]+)":/g, '$1:') + ';');
